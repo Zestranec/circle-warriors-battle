201778a2fc20149c8ffb35855393e6abb879ed5d
Zestranec <ihar.b@outlook.com>
2026-02-26 18:05:51 +0100

    deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/circle-warriors-battle/',
   server: {
     port: 3000,
+    open: true,
   },
   build: {
-    target: 'es2020',
+    target: 'es2022',
+    outDir: 'dist',
   },
 });
